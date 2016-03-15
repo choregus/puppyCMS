d0a2f569028287939e1cc7b44878a254bf23b750
salescoachworld <salescoachworld@gmail.com>
2016-03-15 18:40:22 -0400

    can now choose hamburger or standard menus for site

--- config.php
+++ config.php
@@ -14,11 +14,12 @@ $theme = "united"; # choose from spruce, simplex, amelia, cerulean, cyborg, jour
 # email for forms - this is the email your enquiries will go to.
 $form_email = "your@email.com";
 
-$show_social = 0; # if set to 1, then show social share buttons in side bar.
+$menu_choice = 1; # if set to 1, then show hamburger menu option. 0 will display typical menu.
+$show_social = 1; # if set to 1, then show social share buttons in side bar.
 $show_edit = 1; # if set to 1, then show Admin link in side bar.
 $show_form = 0; # if set to 1, then show an enquiry form in side bar.
 $show_slider = 0; # if set to 1, then show content slider on home page.
-$better_fonts = 1; # if set to 1, then better-sized fonts will be used depending on the display the site is seen on. makes things more readable. WORTH TRYING :) 
+$better_fonts = 1; # if set to 1, then better-sized fonts will be used depending on the display the site is seen on. makes things more readable. WORTH TRYING :)
 
 	# if you do want to show a slider on the home page, put content (such as an image url, or a paragraph of text) in each $slide(x) variable. Max 5. Images must all be the same size.
 	$slide[0] = "";
--- index.php
+++ index.php
@@ -1,17 +1,19 @@
-<?php include('config.php'); $columns =""; ?>
+<?php include('config.php'); ?>
 <!DOCTYPE html>
 <html>
 <head>
 <title><?php echo $title ? $title : $default_title; ?></title>
-<?php 
+<?php
 if (isset($meta_desc)) {echo $meta_desc."\r\n";} // show meta description ?>
 <script src="//cdn.jsdelivr.net/jquery/2.2.0/jquery.min.js"></script>
 <?php
 if ($show_slider == 1 && $_SERVER['REQUEST_URI'] == $site_root) { #show if slider selected  ?><link rel="stylesheet" href="<?php echo $site_root; ?>extras/rs/responsiveslides.css">
 <script src="<?php echo $site_root; ?>extras/rs/responsiveslides.min.js"></script><?php } # end of slider if section ?>
-<link rel="stylesheet" href="//cdn.jsdelivr.net/jquery.sidr/2.2.1/stylesheets/jquery.sidr.light.min.css">
+<?php if ($menu_choice == 1) { ?><link rel="stylesheet" href="//cdn.jsdelivr.net/jquery.sidr/2.2.1/stylesheets/jquery.sidr.light.min.css"><?php } # end menu choice ?>
+
 <meta name="viewport" content="width=device-width,minimum-scale=1">
 <?php if ($better_fonts == 1) { ?><script src="extras/text.js"></script><?php } ?>
+
 </head>
 <body>
 <xmp theme="<?php echo $bootswatch_theme; ?>" style="display:none;">
@@ -27,28 +29,40 @@ foreach ($slide as $value) {
 ?>
 </ul>
 
-<?php } ?>
-<a id="simple-menu" href="#sidr" style="font-size:2em;margin:10px;float:right;">&#9776;</a>
+<?php }
+//show old or new style menus
+$span = ( $menu_choice == 1) ? 'span12' : 'span9';
+?>
+<?php if ($menu_choice == 1) { ?><a id="simple-menu" href="#sidr" style="font-size:2em;margin:10px;float:right;">&#9776;</a><?php } # end menu choice ?>
+
 <div class="row">
-<div class="span12"<?php if ($columns == 2) { ?> style="-webkit-column-width: 500px;-moz-column-width: 500px;column-width: 500px;"<?php } elseif ($columns == 3) { ?> style="-webkit-column-width: 300px;-moz-column-width: 300px;column-width: 300px;"<?php }?>>
-<?php echo $content; ?>
+<div class="<?php echo $span; ?>"<?php if ($columns == 2) { ?> style="-webkit-column-width: 500px;-moz-column-width: 500px;column-width: 500px;"<?php } elseif ($columns == 3) { ?> style="-webkit-column-width: 300px;-moz-column-width: 300px;column-width: 300px;"<?php }?>>
+<?php echo $content;
+
+if ($menu_choice == 0) {
+?>
+</div>
+<div class="span3">
 <?php
+} # end menu choice
+
 # show social buttons if set in config
 if ($show_social == 1) {
 ?>
+
 <div class="don-share" data-style="icons" data-bubbles="none" data-limit="3">
   <div class="don-share-facebook"></div>
   <div class="don-share-twitter"></div>
   <div class="don-share-google"></div>
   <div class="don-share-linkedin"></div>
 </div>
-<?php } 
+<?php } # end of showing social icons
 ?>
-</div>
-<div id="sidr">
+<?php if ($menu_choice == 1) { ?></div><?php } # end menu choice ?>
+<?php if ($menu_choice == 1) { ?><div id="sidr"><?php } # end menu choice ?>
 <?php
 
-# if form has been selected then show	
+# if form has been selected then show
 if ($show_form == 1) {include('extras/form/form-input.html');} # show enquiry form if selected
 //show the list of files in the content directory
 $files = array();
@@ -88,12 +102,12 @@ echo "</ul>\n";
 			
 		});
   });
-</script><?php } # end of slider if section 
+</script><?php } # end of slider if section
 
 # javascript for social buttons
-if ($show_social == 1) {	
+if ($show_social == 1) {
 	?>
-<script type="text/javascript"> 
+<script type="text/javascript">
   (function() {
     var dr = document.createElement('script');
     dr.type = 'text/javascript'; dr.async = true;
@@ -105,10 +119,10 @@ if ($show_social == 1) {
 <script>
 $(document).ready(function() {
   $('#simple-menu').sidr({side: 'right' });
-<?php if ($better_fonts == 1) { ?>	
+<?php if ($better_fonts == 1) { ?>
 	$('.span12').flowtype({minimum   : 299, maximum   : 1500, minFont   : 16, maxFont   : 20, fontRatio : 30 });
 	$('ul').flowtype({minFont   : 16,maxFont   : 18, fontRatio : 30});
-<?php } ?>	
+<?php } ?>
 });
 </script>
 </body>
